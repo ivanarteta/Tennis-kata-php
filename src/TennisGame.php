8797172144160a9a542d49c5d4647dd220823d91
Ivan Arteta <arteta.115674@e.unavarra.es>
2021-03-03 11:30:30 +0100

    [] - añadido test y funcionalidad de Win Player 2

--- src/TennisGame.php
+++ src/TennisGame.php
@@ -11,8 +11,8 @@ class TennisGame
 
     private $player1;
     private $player2;
-    private $player1Score=4;
-    private $player2Score=2;
+    private $player1Score=2;
+    private $player2Score=4;
     private $player1Win = false;
     private $player2Win = false;
 
@@ -60,6 +60,8 @@ class TennisGame
                 return "Love - Forty";
             }else if ($this->player1Score == 4 && $this->player2Score < 3){
                 return "Win Player 1";
+            }else if ($this->player1Score < 3 && $this->player2Score == 4){
+                return "Win Player 2";
             }
 
 
