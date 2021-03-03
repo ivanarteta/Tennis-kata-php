7e44370aaef43658707019624ed9b4d0ea9ceb09
Ivan Arteta <arteta.115674@e.unavarra.es>
2021-03-03 10:46:15 +0100

    [] - añadido test y funcionalidad del estado inicial

--- src/TennisGame.php
+++ src/TennisGame.php
@@ -4,7 +4,32 @@
 namespace Deg540\PHPTestingBoilerplate;
 
 
+use PhpParser\Node\Scalar\String_;
+
 class TennisGame
 {
 
+    private $player1;
+    private $player2;
+    private $player1Score = 0;
+    private $player2Score = 0;
+
+    /**
+     * TennisGame constructor.
+     */
+    public function __construct(String $player1Name, String $player2Name)
+    {
+        $player1 = $player1Name;
+        $player2 = $player2Name;
+    }
+
+    public function wonPoint(String $player){
+
+    }
+
+    public function getScore():string{
+        if($this->player1Score == 0 && $this->player2Score == 0){
+            return "Love all";
+        }
+    }
 }
--- tests/TennisGameTest.php
+++ tests/TennisGameTest.php
@@ -2,9 +2,22 @@
 
 namespace Deg540\PHPTestingBoilerplate\Test;
 
+use Deg540\PHPTestingBoilerplate\TennisGame;
 use PHPUnit\Framework\TestCase;
+use function PHPUnit\Framework\assertEquals;
 
 class TennisGameTest extends TestCase
 {
+    /**
+     * @test
+     **/
+    public function test_del_Estado_inicial()
+    {
+
+        $tennis = new TennisGame("Ivan", "Maider");
+        $output = $tennis->getScore();
+        $this->assertEquals("Love all",$output);
+    }
+
 
 }
