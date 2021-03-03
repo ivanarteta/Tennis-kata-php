f54ec87032437b714043a21c226bfc4b9c2395a0
Ivan Arteta <arteta.115674@e.unavarra.es>
2021-03-03 11:33:31 +0100

    [] - añadido test y funcionalidad de Advantage Player 1

--- src/TennisGame.php
+++ src/TennisGame.php
@@ -11,7 +11,7 @@ class TennisGame
 
     private $player1;
     private $player2;
-    private $player1Score=2;
+    private $player1Score=5;
     private $player2Score=4;
     private $player1Win = false;
     private $player2Win = false;
@@ -62,6 +62,8 @@ class TennisGame
                 return "Win Player 1";
             }else if ($this->player1Score < 3 && $this->player2Score == 4){
                 return "Win Player 2";
+            }else if ($this->player1Score > 3 && $this->player2Score >= 3 && $this->player1Score - $this->player2Score == 1){
+                return "Advantage Player 1";
             }
 
 
