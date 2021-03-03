c1e7a49915f848fc2f49a93cffdb3d0128c534af
Ivan Arteta <arteta.115674@e.unavarra.es>
2021-03-03 10:21:31 +0100

    [] - innitial commit

---
+++ tests/TennisGameTest.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Deg540\PHPTestingBoilerplate\Test;
+
+use PHPUnit\Framework\TestCase;
+
+class TennisGameTest extends TestCase
+{
+
+}
---
+++ tests/Test.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Deg540\PHPTestingBoilerplate\Test;
+
+use FizzBuzzTest;
+use PHPUnit\Framework\TestCase;
+
+class Test extends TestCase
+{
+
+}
